901bbb850aa8da011fe2cc9bc40e9384f21c2320
zhouchenhua <noatnu@foxmail.com>
2023-07-24 18:49:53 +0800

    Default Changelist

--- elasticsearch/src/test/java/com/my/DemoESDoc.java
+++ elasticsearch/src/test/java/com/my/DemoESDoc.java
@@ -6,25 +6,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.lucene.search.TotalHits;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.ShardSearchFailure;
 import org.elasticsearch.client.*;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author : chengdu
@@ -54,13 +66,13 @@ public class DemoESDoc {
         RestHighLevelClient client = getEsHighInit();
         UserEntity userEntity = new UserEntity();
         userEntity.setAddress("中国");
-        userEntity.setAge(RandomUtil.randomInt(1,100));
-        userEntity.setId(RandomUtil.randomInt(1000000,2000000));
+        userEntity.setAge(RandomUtil.randomInt(1, 100));
+        userEntity.setId(RandomUtil.randomInt(1000000, 2000000));
         userEntity.setName(RandomUtil.randomString(5));
         userEntity.setUuid(UUID.fastUUID().toString());
 
         IndexRequest indexRequest = new IndexRequest();
-        indexRequest.index(INDEX_NAME).id(userEntity.getUuid()) ;
+        indexRequest.index(INDEX_NAME).id(userEntity.getUuid());
 
         ObjectMapper mapper = new ObjectMapper();
         String userJson = mapper.writeValueAsString(userEntity);
@@ -77,7 +89,7 @@ public class DemoESDoc {
      * 批量添加
      */
     @Test
-    public void testCreateBatchDoc()throws Exception{
+    public void testCreateBatchDoc() throws Exception {
         RestHighLevelClient client = getEsHighInit();
         // 批量插入数据
         BulkRequest bulkRequest = new BulkRequest();
@@ -86,12 +98,12 @@ public class DemoESDoc {
         for (int i = 0; i < initialCapacity; i++) {
             UserEntity userEntity = new UserEntity();
             userEntity.setAddress("中国");
-            userEntity.setAge(RandomUtil.randomInt(1,100));
-            userEntity.setId(RandomUtil.randomInt(1000000,2000000)+i);
+            userEntity.setAge(RandomUtil.randomInt(1, 100));
+            userEntity.setId(RandomUtil.randomInt(1000000, 2000000) + i);
             userEntity.setName(RandomUtil.randomString(5));
             userEntity.setUuid(UUID.fastUUID().toString());
             userEntity.setBirthday(new Date());
-            bulkRequest.add(new IndexRequest().index(INDEX_NAME).id(userEntity.getId().toString()).source(mapper.writeValueAsString(userEntity),XContentType.JSON));
+            bulkRequest.add(new IndexRequest().index(INDEX_NAME).id(userEntity.getId().toString()).source(mapper.writeValueAsString(userEntity), XContentType.JSON));
         }
         BulkResponse bulk = client.bulk(bulkRequest, RequestOptions.DEFAULT);
         System.out.println(bulk.toString());
@@ -99,6 +111,73 @@ public class DemoESDoc {
         System.out.println(bulk.getItems());
 
         client.close();
+    }
+
+    @Test
+    public void testGetAllDoc() throws Exception {
+        RestHighLevelClient client = getEsHighInit();
+        SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        //可以指定type,也可以不指定，不指定查所有
+//        searchRequest.types("my_type");
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = searchResponse.getHits();
+        long totalHits = hits.getTotalHits().value;
+        //匹配查询的结果集
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit : searchHits) {
+            String sourceAsString = hit.getSourceAsString();
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            System.out.println(sourceAsString);
+        }
+    }
+
+    /**
+     * @param : client
+     * @description : 查询文档————matchAll
+     */
+    public void searchDocumentByMatchAll(RestHighLevelClient client) throws Exception {
+        //可以指定index,也可以不指定，不指定查所有
+        SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        //可以指定type,也可以不指定，不指定查所有
+//        searchRequest.types("my_type");
+
+        SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+
+        //查询信息
+        RestStatus status = searchResponse.status();
+        TimeValue took = searchResponse.getTook();
+        Boolean terminatedEarly = searchResponse.isTerminatedEarly();
+        boolean timedOut = searchResponse.isTimedOut();
+
+        int totalShards = searchResponse.getTotalShards();
+        int successfulShards = searchResponse.getSuccessfulShards();
+        int failedShards = searchResponse.getFailedShards();
+        for (ShardSearchFailure failure : searchResponse.getShardFailures()) {
+            // failures should be handled here
+        }
+
+        SearchHits hits = searchResponse.getHits();
+        long totalHits = hits.getTotalHits().value;
+        float maxScore = hits.getMaxScore();
+
+        //匹配查询的结果集
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit : searchHits) {
+            // do something with the SearchHit
+            String index = hit.getIndex();
+            String type = hit.getType();
+            String id = hit.getId();
+            float score = hit.getScore();
+
+            String sourceAsString = hit.getSourceAsString();
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+        }
     }
 
 

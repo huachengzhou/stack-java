0e3a6786d7e5dbcccac74ac0b71158480f0bb1af
zhouchenhua <noatnu@foxmail.com>
2023-08-15 18:36:49 +0800

    rabbitmq学习

--- rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/v1/Consumer.java
+++ rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/v1/Consumer.java
@@ -1,7 +1,13 @@
 package com.ng.my.v1;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.json.JSONUtil;
 import com.rabbitmq.client.*;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author : chengdu
@@ -9,33 +15,49 @@ import org.junit.Test;
  **/
 public class Consumer {
     private final static String QUEUE_NAME = "hello";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    /**
+     * 简单消费者
+     * @throws Exception
+     */
     @Test
-    public void execute_1() throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("127.0.0.1");
-        factory.setUsername("admin");
-        factory.setPassword("123456");
-        Connection connection = factory.newConnection();
+    public void acceptConsumer() throws Exception {
+        //1:创建连接池
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("123456");
+        Connection connection = connectionFactory.newConnection();
+
+        //2:创建通道(数据交换通道)
         Channel channel = connection.createChannel();
-        System.out.println("等待接收消息....");
-        //推送的消息如何进行消费的接口回调
-        DeliverCallback deliverCallback=(consumerTag,delivery)->{
-            String message= new String(delivery.getBody());
-            System.out.println(message);
+        logger.info(DateUtil.now() +"等待接收消息....");
+
+        //3:推送的消息如何进行消费的接口回调
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            logger.info("接收时间:"+DateUtil.now());
+            //consumerTag 消费者标签，用来区分多个消费者
+            logger.info("consumerTag:" + consumerTag);
+            logger.info("properties:" + JSONUtil.toJsonStr(message.getProperties()));
+            logger.info("envelope:" + JSONUtil.toJsonStr(message.getEnvelope()));
+            logger.info("message:" + new String(message.getBody()));
+            logger.info("");
         };
-        //取消消费的一个回调接口 如在消费的时候队列被删除掉了
-        CancelCallback cancelCallback=(consumerTag)->{
-            System.out.println("消息消费被中断");
+        //4:取消消费的一个回调接口 如在消费的时候队列被删除掉了
+        CancelCallback cancelCallback = consumerTag -> {
+            //consumerTag 消费者标签，用来区分多个消费者
+            logger.info("consumerTag:" + consumerTag);
         };
+
         /**
          * 消费者消费消息
          * 1.消费哪个队列
          * 2.消费成功之后是否要自动应答 true 代表自动应答 false 手动应答
          * 3.消费者未成功消费的回调
          */
-        channel.basicConsume(QUEUE_NAME,true,deliverCallback,cancelCallback);
+        channel.basicConsume(QUEUE_NAME,true,deliverCallback,cancelCallback) ;
+        TimeUnit.MINUTES.sleep(2);
     }
-
 
 }
--- rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/v1/Producer.java
+++ rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/v1/Producer.java
@@ -1,9 +1,13 @@
 package com.ng.my.v1;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author : chengdu
@@ -11,36 +15,45 @@ import org.junit.Test;
  **/
 public class Producer {
     private final static String QUEUE_NAME = "hello";
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 简单生产者
+     * @throws Exception
+     */
     @Test
-    public void execute_1() throws Exception {
-        //创建一个连接工厂
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("127.0.0.1");
-        factory.setUsername("admin");
-        factory.setPassword("123456");
+    public void acceptProducer()throws Exception{
+        //1:创建连接池
+        ConnectionFactory connectionFactory = new ConnectionFactory() ;
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("123456");
+
+        Connection connection = connectionFactory.newConnection();
+        //2:创建通道(数据交换通道)
         //channel 实现了自动 close 接口 自动关闭 不需要显示关闭
-        try (Connection connection = factory.newConnection(); Channel channel =
-                connection.createChannel()) {
-            /**
-             * 生成一个队列
-             * 1.队列名称
-             * 2.队列里面的消息是否持久化 默认消息存储在内存中
-             * 3.该队列是否只供一个消费者进行消费 是否进行共享 true 可以多个消费者消费
-             * 4.是否自动删除 最后一个消费者端开连接以后 该队列是否自动删除 true 自动删除
-             * 5.其他参数
-             */
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "hello world";
-            /**
-             * 发送一个消息
-             * 1.发送到那个交换机
-             * 2.路由的 key 是哪个
-             * 3.其他的参数信息
-             * 4.发送消息的消息体
-             */
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-            System.out.println("消息发送完毕");
-        }
+        Channel channel = connection.createChannel();
+
+        /**
+         * 生成一个队列
+         * 1.队列名称
+         * 2.队列里面的消息是否持久化 默认消息存储在内存中
+         * 3.该队列是否只供一个消费者进行消费 是否进行共享 true 可以多个消费者消费
+         * 4.是否自动删除 最后一个消费者端开连接以后 该队列是否自动删除 true 自动删除
+         * 5.其他参数
+         */
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null) ;
+        String message = "hello world "+ RandomUtil.randomInt(10,10000);
+        /**
+         * 发送一个消息
+         * 1.发送到那个交换机
+         * 2.路由的 key 是哪个
+         * 3.其他的参数信息
+         * 4.发送消息的消息体
+         */
+        channel.basicPublish("",QUEUE_NAME,null,message.getBytes("UTF-8"));
+        logger.info("message:"+message);
+        logger.info(DateUtil.now() +"发送消息!");
     }
 
 }

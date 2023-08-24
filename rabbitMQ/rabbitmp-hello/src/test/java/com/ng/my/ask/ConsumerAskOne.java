7f24eb5b65f9d403a98e475a781d85f21655e01d
zhouchenhua <noatnu@foxmail.com>
2023-08-24 18:16:09 +0800

    rabbitmq学习

--- rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/ask/ConsumerAskOne.java
+++ rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/ask/ConsumerAskOne.java
@@ -1,2 +1,66 @@
-package com.ng.my.ask;public class ConsumerAskOne {
+package com.ng.my.ask;
+
+import com.my.common.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class ConsumerAskOne {
+
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private static final String ACK_QUEUE_NAME = "ack_queue";
+
+    @Test
+    public void executeAsk() throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+
+        logger.info("C1 等待接收消息处理时间较短");
+
+        extracted(channel,1);
+
+        TimeUnit.MINUTES.sleep(3);
+        logger.info("end");
+    }
+
+    @Test
+    public void executeTwoAsk() throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+
+        logger.info("C2 等待接收消息处理时间较短");
+
+        extracted(channel,30);
+
+        TimeUnit.MINUTES.sleep(3);
+        logger.info("end");
+    }
+
+    private void extracted(Channel channel,long timeout) throws IOException {
+        //消息消费的时候如何处理消息
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody());
+            try {
+                TimeUnit.SECONDS.sleep(timeout);
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
+            }
+            System.out.println("接收到消息:" + message);
+            /**
+             * 1.消息标记 tag
+             * 2.是否批量应答未应答消息
+             */
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        };
+        //采用手动应答
+        boolean autoAck = false;
+        channel.basicConsume(ACK_QUEUE_NAME, autoAck, deliverCallback, (consumerTag) -> {
+            System.out.println(consumerTag + "消费者取消消费接口回调逻辑");
+        });
+    }
+
 }
--- rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/ask/ProducerAskOne.java
+++ rabbitMQ/rabbitmp-hello/src/test/java/com/ng/my/ask/ProducerAskOne.java
@@ -1,2 +1,42 @@
-package com.ng.my.ask;public class ProducerAskOne {
+package com.ng.my.ask;
+
+import com.my.common.RabbitMqUtils;
+import com.rabbitmq.client.Channel;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class ProducerAskOne {
+
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private static final String TASK_QUEUE_NAME = "ack_queue";
+
+    @Test
+    public void acceptProducer()throws Exception{
+        Channel channel = RabbitMqUtils.getChannel();
+
+        //创建一个队列
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+
+
+        //从控制台当中接受信息
+        logger.info("start");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String message = scanner.next();
+            logger.info(" [x] Sent '" + message + "'");
+            channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes("UTF-8"));
+
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+
+        TimeUnit.MINUTES.sleep(2);
+        logger.info("end");
+    }
+
+
 }
